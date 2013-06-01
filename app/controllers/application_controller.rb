9d893d72672d213468d5d1ecd6a7723caa95bf9c
Fernando Angarita <fangari@gmail.com>
2013-06-01 16:53:24 -0500

    Add authentication lock down

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  # before_filter :authenticated_user
-
-  def authenticated_user
-    unless user_signed_in?
-      redirect_to root_url
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,24 @@
 VisionBoard20::Application.routes.draw do
 
-  root to: "high_voltage/pages#show", id: 'index'
+  # root to: "high_voltage/pages#show", id: 'index'
+  unauthenticated do
+    root to: "high_voltage/pages#show", id: 'index'
+  end
+
   devise_for :users
-  resources :soundtracks
-  resources :playlists
-  resources :photo_arrays
-  resources :photo_libraries
-  resources :slideshows
-  resources :songs
-  resources :photographs
+
+  authenticated :user do
+    root to: "Slideshows#index"
+  end
+
+  authenticate do
+    resources :soundtracks
+    resources :playlists
+    resources :photo_arrays
+    resources :photo_libraries
+    resources :slideshows
+    resources :songs
+    resources :photographs
+  end
   resources :usersend
 end
